2bad412a3a4c39f1363fe59c8a2181101d34b606
Alexander Lopez <alexanderlopezormaza@gmail.com>
2025-07-14 18:05:10 +0200

    ejercicio contar 1-100 1x1 bucle

---
+++ contar_1_al_100_bucle.py
@@ -0,0 +1,5 @@
+print("=== Método 4: 10 números por línea ===")
+for i in range(1, 101):
+    print(f"{i:3d}", end=" ")  # :3d formatea con 3 espacios
+    if i % 10 == 0:  # Salto de línea cada 10 números
+        print()  # Nueva línea
