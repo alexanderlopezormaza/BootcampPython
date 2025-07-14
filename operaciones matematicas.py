5ed4fa92bb7f411177036c750c0f0c5a6a4b9311
Alexander Lopez <alexanderlopezormaza@gmail.com>
2025-07-14 17:20:27 +0200

    dia 11

--- operaciones matematicas.py
+++ operaciones matematicas.py
@@ -15,21 +15,21 @@ def operaciones(N1, N2):
     num1 = N1
     num2 = N2
     resultado = 0
-    listaResultados = (0,0,0,0)
+    listaResultados=(0,0,0,0)
 
     if isinstance(num1, numbers.Number) and isinstance(num2, numbers.Number):
 
         #Suma de dos valores (+)
-        listaResultados(0) = num1 + num2
+        listaResultados[0] = num1 + num2
         
         #Resta de dos valores (-)
-        listaResultados(1) = num1 - num2
+        listaResultados[1] = num1 - num2
 
         #Multiplicacion de dos valores (*)
-        listaResultados(2) = num1 * num2
+        listaResultados[2] = num1 * num2
 
         #Division de dos valores (/)
-        listaResultados(3) = num1 / num2 
+        listaResultados[3] = num1 / num2 
 
         for i in range (0, len(listaResultados)):
             print("El resultado de la operacion es..")
